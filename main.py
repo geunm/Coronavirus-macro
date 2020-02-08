079e46b2fa1ed564d7a877825562a856bbf61c64
geunm <kimgeun3182@gmail.com>
2020-02-08 15:29:19 +0900

    add day-hour-time

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import time      # 30분 기다리기
 
 page=(30,193)
 
-pyautogui.screenshot('./confirmed/1.png', region=(22,195,130,80))
-pyautogui.screenshot('./deaths/1.png', region=(698,201,120,80))
-pyautogui.screenshot('./recovered/1.png', region=(841,205,100,80))
+while True:
+    pyautogui.screenshot('./confirmed/' + time.strftime('%d-%H-%M', time.localtime(time.time())) + '.png', region=(22,195,130,80))
+    pyautogui.screenshot('./deaths/' + time.strftime('%d-%H-%M', time.localtime(time.time()))+ '.png', region=(698,201,120,80))
+    pyautogui.screenshot('./recovered/'+ time.strftime('%d-%H-%M', time.localtime(time.time()))+ '.png', region=(841,205,100,80))
 
-pyautogui.click(page)
-pyautogui.typewrite(['F5'])
+    pyautogui.click(page)
+    pyautogui.typewrite(['F5'])
+
+    time.sleep(1800) # 30분 
